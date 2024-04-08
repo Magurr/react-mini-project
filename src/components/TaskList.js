277d63581b75d29d01a4629211637b7cd5899bdd
İnanç Topuz <inanctopuz@hotmail.com>
2024-04-08 16:38:05 +0300

    Initial commit

---
+++ src/components/TaskList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import TaskShow from "./TaskShow";
+
+function TaskList({ tasks, onDelete, onUpdate }) {
+  return (
+    <div className="task-list">
+      {tasks.map((task, key) => {
+        return (
+          <TaskShow
+            key={task.id}
+            task={task}
+            onDelete={onDelete}
+            onUpdate={onUpdate}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default TaskList;
